784977f4f84832bf76ed3f3b72312b35618a2b86
tuannm <nminh.tuan106@gmail.com>
2023-10-31 14:30:22 +0700

    WIP: document

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>FPT Blockchain Lab</span>,
+  project: {
+    link: "https://github.com/FPT-Blockchain-Lab",
+  },
+  footer: {
+    text: "FPT Blockchain Lab",
+  },
+  darkMode: false,
+};
+
+export default config;
